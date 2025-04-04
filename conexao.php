394848b1cf10d0268b0ccbeb7a0ba223021eec5c
deboralbupaiva <166133201+deboralbupaiva@users.noreply.github.com>
2025-04-04 12:09:49 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -5,7 +5,7 @@ $senha = '';
 $database = 'usuarios';
 $host = 'localhost';
 
-$mysqli = new mysqli($host,$usuarios,$senha,$database)
+$mysqli = new mysqli($host,$usuario,$senha,$database);
 
 if($mysqli->connect_error){
     die("Falha ao conectar ao banco de dados" . $mysqli->connect_error);
--- index.php
+++ index.php
@@ -1,6 +1,46 @@
 <?php
+//Incluir o arquivo de conexão de banco
 include('conexao.php');
 
+//Verifiva se os campos emai e senha foram enviados via POST
+if(isset($_POST['email']) || isset($_POST['senha']))
+{
+
+    //verificar se o campo email esta vazio
+    if(strlen($_POST['email'] == 0)){
+        echo "Preencha seu e-mail";
+    }
+    //verificar se o campo senha esta vazio
+    else if(strlen($_POST['senha'] == 0)){
+        echo "Preencha sua senha";
+    }
+    else {
+        //Proteje contra SQL Injection escapando caracteres especiais
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
+
+        //Consulta no banco de dados se exitem o usuario e senha
+        $sql_code = "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL:" . $mysqli->error);
+
+        //Obtém o número de registros encontrados
+        $quantidade = $sql_query->num_rows;
+
+        if($qusntidade == 1){
+            //Obtém os dados do usuário
+            $usuario = $sql_query->fetch_assoc();
+            
+            //Inicia a sessão, caso ainda não tenha sido iniciada
+            if(!isset($_SESSION)){
+                session_start();
+            }
+            
+            //Armazena informações do usuário na sessão 
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+        }
+    }
+}
 
 ?>
 
